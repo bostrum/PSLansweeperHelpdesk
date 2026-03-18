7cfe1797701e3af6c592895ac14d5120988767c1
Felix Boström <46707048+bostrum@users.noreply.github.com>
2026-03-18 10:12:57 +0100

    custom field removal added

---
+++ PSLansweeperHelpDesk/Functions/Remove-LSTicketCustomField.ps1
@@ -0,0 +1,32 @@
+
+function Remove-LSTicketCustomField {
+    
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$Url,
+
+        [Parameter(Mandatory=$true)]
+        [String]$TicketID,
+        
+        [Parameter(Mandatory=$true)]
+        [String]$CustomFieldName
+    )
+
+    # API call to remove custom field
+    $Url = ($Url + "action=DeleteTicketCustomField&" + "TicketID=$TicketID&" + "CustomFieldName=$CustomFieldName&")
+    $Request = Invoke-WebRequest -Uri $Url -UseBasicParsing
+    
+    # Checking response
+    if($Request.StatusCode -ne 200) {
+        throw ("{0} {1}" -f $Request.StatusCode, $Request.StatusDescription)
+    }
+    
+    # Success check and data return
+    $Data = $Request.Content | ConvertFrom-Json
+    if($Data.Success) {
+        return $Data
+    } else {
+        Write-Error "Removing custom field failed, check output message."
+        return $Data
+    }
+}
