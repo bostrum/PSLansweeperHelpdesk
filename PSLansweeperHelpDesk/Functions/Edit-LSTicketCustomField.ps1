5f285d0158ba4af8982c1d3db9b9d21f8ed1fad8
Felix Boström <46707048+bostrum@users.noreply.github.com>
2026-03-18 10:10:22 +0100

    custom field edit added

---
+++ PSLansweeperHelpDesk/Functions/Edit-LSTicketCustomField.ps1
@@ -0,0 +1,50 @@
+
+function Edit-LSTicketCustomField {
+    
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$Url,
+
+        [Parameter(Mandatory=$true)]
+        [String]$TicketID,
+        
+        [Parameter(Mandatory=$true)]
+        [String]$CustomFieldName,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Value')]
+        [string]$CustomFieldValue,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Values')]
+        [string]$CustomFieldValues,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'JSON')]
+        [string]$CustomFieldJSON
+    )
+    
+    # Checking value type used
+    $Asset = if($PSCmdlet.ParameterSetName -eq 'Value') {
+        "CustomFieldValue=$CustomFieldValue"
+    } elseif($PSCmdlet.ParameterSetName -eq 'Values') {
+        "CustomFieldValues=$CustomFieldValues"
+    } elseif($PSCmdlet.ParameterSetName -eq 'JSON') {
+        "CustomFieldJSON=$CustomFieldJSON"
+    }
+    
+    # API call to edit custom field
+    $Url = ($Url + "action=EditTicketCustomField&" + "TicketID=$TicketID&" + "CustomFieldName=$CustomFieldName&" + $Asset) 
+    $Request = Invoke-WebRequest -Uri $Url -UseBasicParsing
+    
+    # Checking response
+    if($Request.StatusCode -ne 200) {
+        throw ("{0} {1}" -f $Request.StatusCode, $Request.StatusDescription)
+    }
+    
+    # Success check and data return
+    $Data = $Request.Content | ConvertFrom-Json
+    if($Data.Success) {
+        return $Data
+    } else {
+        Write-Error "Editing custom field failed, check output message."
+        return $Data
+    }
+}
