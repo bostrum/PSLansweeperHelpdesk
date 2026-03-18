6f8a2686ffadc3345e0b133bfe3f16a318a535ed
Felix Boström <46707048+bostrum@users.noreply.github.com>
2026-03-18 10:35:57 +0100

    made username or email mandatory

--- PSLansweeperHelpDesk/Functions/Add-LSTicket.ps1
+++ PSLansweeperHelpDesk/Functions/Add-LSTicket.ps1
@@ -6,13 +6,17 @@ function Add-LSTicket {
         [Parameter(Mandatory=$true)][String]$Subject,
         [Parameter(Mandatory=$true)][String]$Description,
 
+        # Mandatory to use one
+        [Parameter(Mandatory = $true, ParameterSetName = 'Username')]
+        [String]$Username,
+        [Parameter(Mandatory = $true, ParameterSetName = 'Email')]
+        [String]$Email,
+
         # Non-mandatory fields
         [String]$Type,
         [String]$Priority,
         [String]$Team,
-        [String]$Username,
         [String]$Displayname,
-        [String]$Email,
         [String]$AgentUsername,
         [String]$AgentEmail,
         [DateTime]$Date,
@@ -28,14 +32,19 @@ function Add-LSTicket {
         Subject     = $Subject
         Description = $Description
     }
+
+    # Username or Email for user
+    $User = if($PSCmdlet.ParameterSetName -eq 'Username') {
+        $params.Username = $Username
+    } else {
+        $params.Email = $Email
+    }
     
     # Add optional parameters if used
     if($Type)           {$params.Type           = $Type}
     if($Priority)       {$params.Priority       = $Priority}
     if($Team)           {$params.Team           = $Team}
-    if($Username)       {$params.Username       = $Username}
     if($Displayname)    {$params.Displayname    = $Displayname}
-    if($Email)          {$params.Email          = $Email}
     if($AgentUsername)  {$params.AgentUsername  = $AgentUsername}
     if($AgentEmail)     {$params.AgentEmail     = $AgentEmail}
     if($Date)           {$params.Date           = $Date}
