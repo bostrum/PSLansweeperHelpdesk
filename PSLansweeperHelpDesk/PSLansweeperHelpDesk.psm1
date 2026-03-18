99bdb36f294c00493846120939acc8026fcde52a
Felix Boström <46707048+bostrum@users.noreply.github.com>
2026-03-18 13:08:32 +0100

    added try/catch

--- PSLansweeperHelpDesk/PSLansweeperHelpDesk.psm1
+++ PSLansweeperHelpDesk/PSLansweeperHelpDesk.psm1
@@ -1,8 +1,12 @@
 
-$functions  = Get-ChildItem -Path "$PSScriptRoot\Functions\*.ps1" -Recurse
+$functions  = Get-ChildItem -Path "$PSScriptRoot\Functions\*.ps1" -Recurse -ErrorAction SilentlyContinue
 
 foreach ($file in $functions) {
+    try {
     . $file.FullName
+    } catch {
+        Write-Error "Error when importing $($file.fullname): $_"
+    }
 }
 
 Export-ModuleMember -Function $functions.BaseName
